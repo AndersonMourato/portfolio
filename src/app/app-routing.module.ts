918366837ade1c3022ceeee5398509f1f5fa144e
Anderson Inacio <anderson.mouratoo@gmail.com>
2024-03-17 18:50:04 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErroComponent } from './erro/erro.component';
 
 const routes: Routes = [
+  {path:"", component: HomeComponent },
   {path:"home", component: HomeComponent },
-  {path:"about", component: HomeComponent },
-  {path:"projects", component: HomeComponent },
-  {path:"contact", component: HomeComponent },
-  {path:"**", component: HomeComponent },
-  {path:"", redirectTo:"home", pathMatch: "full"},
+  {path:"about", component: AboutComponent },
+  {path:"projects", component: ProjectsComponent },
+  {path:"contact", component: ContactComponent },
+  {path:"**", component: ErroComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,29 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErroComponent } from './erro/erro.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    ErroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
